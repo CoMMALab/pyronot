f6f45ba9f9ef0112579bc26425a9b920e079c622
Sai <sai.c.coumar1@gmail.com>
2026-04-16 12:34:33 -0400

    added some extra functionality for region based IK

--- src/pyronot/optimization_engines/__init__.py
+++ src/pyronot/optimization_engines/__init__.py
@@ -1,6 +1,7 @@
 from ._hjcd_ik import hjcd_solve as hjcd_solve
 from ._ls_ik import ls_ik_solve as ls_ik_solve
 from ._ls_ik import ls_ik_solve_cuda as ls_ik_solve_cuda
+from ._ls_ik import ls_ik_solve_cuda_batch as ls_ik_solve_cuda_batch
 from ._sqp_ik import sqp_ik_solve as sqp_ik_solve
 from ._sqp_ik import sqp_ik_solve_cuda as sqp_ik_solve_cuda
 from ._mppi_ik import mppi_ik_solve as mppi_ik_solve
